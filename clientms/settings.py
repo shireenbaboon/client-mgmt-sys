6789a581c8d98ea65bf5b54c4d436fd0733b3d3c
shireenbaboon <shireen54@gmail.com>
2021-09-05 16:14:18 -0500

    added .gitignore file

--- clientms/settings.py
+++ clientms/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#^k77@d^n(%j(!(=!kbki5lha4dg%decy52kt=pft6li8j@zvi'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -124,6 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+LOGIN_REDIRECT_URL = '/'
+
 
 AUTH_USER_MODEL = 'users.CustomUser'
 LOGIN_REDIRECT_URL = 'home'
